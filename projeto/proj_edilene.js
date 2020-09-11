8b7ef2079f7f002f92b6742e5b29da843bccb5b2
edilenelopes <lopes_edi@yahoo.com.br>
2020-09-11 16:22:29 -0300

    Inclusão da soma dos itens comprados

--- projeto/proj_edilene.js
+++ projeto/proj_edilene.js
@@ -7,7 +7,7 @@ const bancoDados = require('./database')
 
 
 //fazendo desestruturação
-const {produtos} = bancoDados
+const { produtos } = bancoDados
 
 const listaDeProdutos = []
 
@@ -17,71 +17,73 @@ console.table(produtos)
 
 const read = require('readline-sync')
 
+novaLet = 0
 
-function compra() {
-    do {
-        const numId = parseInt(read.question('Por favor, informe o id do produto desejado: '))
-        
-        const qtdade = parseInt(read.question('Informe a quantidade : '))
+do {
+  const numId = parseInt(read.question('Por favor, informe o id do produto desejado: '))
 
-        //testar se a quantidade é positivo
-        if(qtdade <= 0){
-            console.log('Informe uma quantidade válida.')
-        }
-        //função para encontrar id do produto
-        let produtoEncontrado = produtos.find(elemento => elemento.id === numId)
+  const qtdade = parseInt(read.question('Informe a quantidade : '))
 
-        //testar se id existe
-        if(produtoEncontrado === undefined) {
-            console.log('Id não encontrado, digite novamente.')
-        }else {
-            const produtoPedido = { ...produtoEncontrado, quantidade: qtdade} // se existir, vai para um array
-              listaDeProdutos.push(produtoPedido) //adiciona no array lista de produtos             
-            }
-            
-            confirmar = read.question('Continuar comprando? (S/N) ') //encerra a compra?
-    }while (confirmar.toLowerCase() === 's')
+  
+  //função para encontrar id do produto
+  let produtoEncontrado = produtos.find(elemento => elemento.id === numId)
 
-}
-      
-//chamar função compra
-compra()
-
-class Pedido {
-    constructor(listaDeProdutos){
-      this.products = listaDeProdutos 
-      this.data = new Date()
-      this.subtotal = 0 // Guarda o resultado da função "calcularSubtotal"
-      this.discount = 0 // guarda o resultado da função 'calcular desconto'
-      this.total = 0 //guarda o valor total da compra
-    }
-    //calculando o preço * quantidade de cada item das compras e no final retorna o 
-    //valor total dos itens bruto, ou seja sem desconto
-
-    calcularSubtotal() { 
-      this.subtotal = this.products.reduce((acumulador, item) => acumulador + (item.preco * item.quantidade), 0)
-    }
-
-    //preço apenas do desconto
-    calcularDesconto() {
-        this.discount = (cupom > 0 && cupom <= 15) ? this.subtotal * (cupom / 100).toFixed(2) : 0
-    }
-    
-    //calculando o valor total menos o desconto
-    calcularTotal() {
-        this.total = this.subtotal - this.discount
-    }
+  //testar se a quantidade é positivo
+  if (qtdade <= 0) {
+    console.log('Informe uma quantidade válida.')
   }
 
- 
-const pedido = new Pedido (listaDeProdutos)// Joga o array com os push dentro da classe "Pedido"
+  //testar se id existe
+  if (produtoEncontrado === undefined) {
+    console.log('Id não encontrado, digite novamente.')
+  } else {
+    const produtoPedido = { ...produtoEncontrado, quantidade: qtdade } // se existir, vai para um array
+    listaDeProdutos.push(produtoPedido) //adiciona no array lista de produtos     
+    novaLet = novaLet + produtoPedido.quantidade
+  }
+
+  confirmar = read.question('Continuar comprando? (S/N) ') //encerra a compra?
+} while (confirmar.toLowerCase() === 's')
+
+
+//criando classe com a lista de produtos co carrinho
+class Pedido {
+  constructor(listaDeProdutos) {
+    this.products = listaDeProdutos
+    this.data = new Date()
+    this.subtotal = 0 // Guarda o resultado da função "calcularSubtotal"
+    this.discount = 0 // guarda o resultado da função 'calcular desconto'
+    this.total = 0 //guarda o valor total da compra
+  }
+  //calculando o preço * quantidade de cada item das compras e no final retorna o 
+  //valor total dos itens bruto, ou seja sem desconto
+
+  calcularSubtotal() {
+    this.subtotal = this.products.reduce((acumulador, item) => acumulador + (item.preco * item.quantidade), 0)
+  }
+
+  //preço apenas do desconto
+  calcularDesconto() {
+    this.discount = (cupom > 0 && cupom <= 15) ? this.subtotal * (cupom / 100).toFixed(2) : 0
+  }
+
+  //calculando o valor total menos o desconto
+  calcularTotal() {
+    this.total = this.subtotal - this.discount
+  }
+  
+}
+
+const pedido = new Pedido(listaDeProdutos)// Joga o array com os push dentro da classe "Pedido"
 console.table(pedido.products) // Verificando se deu certo o array dentro da classe 
 
+console.log(`Você comprou ${novaLet} itens.`)
+
 pedido.calcularSubtotal() // chamando a função "calcularSubtotal"
-console.log(pedido.subtotal) 
+console.log(`O subtotal do pedido é R$ `, pedido.subtotal)
 
 //- receber via entrada o cupom de desconto
-const cupom = parseInt(read.question("Informe o valor do cupom de desconto: ")) 
+const cupom = parseInt(read.question("Informe o valor do cupom de desconto: "))
 
 //- Chamar a função de desconto
 pedido.calcularDesconto()
@@ -89,5 +91,5 @@ console.log(`Valor do desconto: R$ ${pedido.discount}`)
 
 //chamar função de valor total
 pedido.calcularTotal()
-console.log(`O valor total da comrpa é R$ ${pedido.total}`)
+console.log(`O valor total da compra é R$ ${pedido.total}`)
 
